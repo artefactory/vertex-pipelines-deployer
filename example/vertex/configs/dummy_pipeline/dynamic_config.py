6f914292bf5c51c7256c5f88d2e102ea42db67ff
julesbertrand <julesbertrand13@gmail.com>
2023-10-02 11:57:29 +0200

    feat: add python config file example

---
+++ example/vertex/configs/dummy_pipeline/dynamic_config.py
@@ -0,0 +1,7 @@
+parameter_values = {
+    "name": "foo",
+}
+
+input_artifacts = {
+    # "input": "gs://foo/bar",  # uncomment this to raise validation error when checking dummy_pipeline  # noqa: E501
+}
