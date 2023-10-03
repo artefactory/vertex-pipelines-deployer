2cdca4571a5b66dd3909882ca7f6dc0102d5a25b
julesbertrand <julesbertrand13@gmail.com>
2023-10-03 16:11:31 +0200

    enh: use dataclass as row for pipeline checks and group errors by config path

--- deployer/utils/utils.py
+++ deployer/utils/utils.py
@@ -9,6 +9,7 @@ from pydantic import ValidationError
 from rich.table import Table
 
 from deployer.utils.logging import console
+from deployer.utils.models import ChecksTableRow
 
 
 def make_enum_from_python_package_dir(dir_path: Path, raise_if_not_found: bool = False) -> Enum:
@@ -51,11 +52,14 @@ def import_pipeline_from_dir(dirpath: Path, pipeline_name: str) -> graph_compone
     return pipeline
 
 
-def print_check_results_table(to_check: dict[str, list], validation_error: ValidationError = None):
-    """This function prints a table of check results.
+def print_check_results_table(
+    to_check: dict[str, list], validation_error: Optional[ValidationError] = None
+) -> None:
+    """This function prints a table of check results to the console.
 
     Args:
-        to_check (dict[str, list]): A dictionary containing the pipelines to check.
+        to_check (dict[str, list]): A dictionary containing the pipelines to check
+            as keys and the config filepaths as values.
         validation_error (ValidationError): The validation error if any occurred during the check.
     """
     val_error_dict = validation_error.errors() if validation_error else {}
@@ -63,51 +67,72 @@ def print_check_results_table(to_check: dict[str, list], validation_error: Valid
         p: [v for v in val_error_dict if v["loc"][1] == p] for p in to_check.keys()
     }
 
-    table = Table(
-        show_header=True,
-        header_style="bold",
-        show_lines=True,
-    )
+    table = Table(show_header=True, header_style="bold", show_lines=True)
 
+    table.add_column("Status", justify="center")
     table.add_column("Pipeline")
     table.add_column("Pipeline Error Message")
     table.add_column("Config Path")
-    table.add_column("Config Error Type")
     table.add_column("Attribute")
+    table.add_column("Config Error Type")
     table.add_column("Config Error Message")
 
     for pipeline_name, config_filepaths in to_check.items():
         errors = parsed_val_error_dict[pipeline_name]
         if len(errors) == 0:
             for config_filepath in config_filepaths:
-                table.add_row(
-                    pipeline_name, None, config_filepath.name, None, None, None, style="green"
+                row = ChecksTableRow(
+                    status="✅",
+                    pipeline=pipeline_name,
+                    config_path=config_filepath.name,
                 )
+                table.add_row(*row.model_dump().values(), style="green")
             if len(config_filepaths) == 0:
-                table.add_row(
-                    pipeline_name, None, "No configs found", None, None, None, style="bold yellow"
+                row = ChecksTableRow(
+                    status="⚠️",
+                    pipeline=pipeline_name,
+                    config_path="No configs found",
                 )
+                table.add_row(*row.model_dump().values(), style="bold yellow")
+
         elif len(errors) == 1 and len(errors[0]["loc"]) == 2:
-            table.add_row(pipeline_name, errors[0]["msg"], None, None, None, None, style="red")
+            print(errors)
+            row = ChecksTableRow(
+                status="❌",
+                pipeline=pipeline_name,
+                pipeline_error_message=errors[0]["msg"],
+                config_path="Could not check config files due to pipeline error.",
+            )
+            table.add_row(*row.model_dump().values(), style="red")
+
         else:
             for config_filepath in config_filepaths:
-                printed = False
+                error_rows = []
                 for error in errors:
                     if error["loc"][3] == config_filepath.name:
-                        table.add_row(
-                            pipeline_name,
-                            None,
-                            config_filepath.name,
-                            error["type"],
-                            error["loc"][4],
-                            error["msg"],
-                            style="red",
-                        )
-                        printed = True
-                if not printed:
-                    table.add_row(
-                        pipeline_name, None, config_filepath.name, None, None, None, style="green"
+                        error_row = {
+                            "type": error["type"],
+                            "attribute": error["loc"][4],
+                            "msg": error["msg"],
+                        }
+                        error_rows.append(error_row)
+                if error_rows:
+                    row = ChecksTableRow(
+                        status="❌",
+                        pipeline=pipeline_name,
+                        config_path=config_filepath.name,
+                        config_error_type="\n".join([er["type"] for er in error_rows]),
+                        attribute="\n".join([er["attribute"] for er in error_rows]),
+                        config_error_message="\n".join([er["msg"] for er in error_rows]),
                     )
+                    table.add_row(*row.model_dump().values(), style="red")
+                else:
+                    row = ChecksTableRow(
+                        status="✅",
+                        pipeline=pipeline_name,
+                        config_path=config_filepath.name,
+                    )
+                    table.add_row(*row.model_dump().values(), style="green")
 
     table.columns = [c for c in table.columns if "".join(c._cells) != ""]
 
