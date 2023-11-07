8883a5fa90140e2c59db8ff7e15274ce3159033e
julesbertrand <julesbertrand13@gmail.com>
2023-11-07 10:53:05 +0100

    tests: fix tests results

--- tests/unit_tests/test_models.py
+++ tests/unit_tests/test_models.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 import pytest
 
 from deployer.pipeline_checks import _convert_artifact_type_to_str
@@ -7,11 +9,11 @@ from deployer.utils.models import CustomBaseModel, create_model_from_func
 class TestCreateModelFromPipeline:
     def test_create_model_from_func_success(self):
         # Given
-        def func(a: int | None, b: str) -> bool:
+        def func(a: Optional[int], b: str) -> bool:
             return True
 
         expected_properties = {
-            "a": {"title": "A", "type": "integer"},
+            "a": {"anyOf": [{"type": "integer"}, {"type": "null"}], "title": "A"},
             "b": {"title": "B", "type": "string"},
         }
 
@@ -32,7 +34,7 @@ class TestCreateModelFromPipeline:
 
         # Then
         assert issubclass(result, CustomBaseModel)
-        assert result.__name__ == "dummy-pipeline"
+        assert result.__name__ == "dummy_pipeline"
 
     def test_create_model_from_func_none_pipeline(self):
         # Given
@@ -49,7 +51,7 @@ class TestCreateModelFromPipeline:
 
         # When / Then
         with pytest.raises(TypeError):
-            create_model_from_func(pipeline)
+            create_model_from_func(pipeline.pipeline_func)
 
     def test_create_model_from_func_types_are_good(self, dummy_pipeline_fixture):
         # Given
@@ -60,7 +62,9 @@ class TestCreateModelFromPipeline:
         }
 
         # When
-        result = create_model_from_func(pipeline, type_converter=_convert_artifact_type_to_str)
+        result = create_model_from_func(
+            pipeline.pipeline_func, type_converter=_convert_artifact_type_to_str
+        )
 
         # Then
         assert result.model_json_schema()["properties"] == expected_properties
