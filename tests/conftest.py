3ae7b81bbfee9c85bc261ffb0b667514fea318be
julesbertrand <julesbertrand13@gmail.com>
2023-11-24 16:58:01 +0100

    test: add deployer tests for method with api request

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,24 @@
+import pickle
+
 import kfp.dsl
 import pytest
 from kfp.dsl import Artifact, Input
 
 
 @pytest.fixture
-def dummy_pipeline_fixture():
+def test_pipeline_fixture():
     @kfp.dsl.component(base_image="python:3.10-slim-buster")
     def dummy_component(name: str, artifact: Input[Artifact]) -> None:
         print("Hello ", name)
 
     @kfp.dsl.pipeline(name="dummy_pipeline")
-    def dummy_pipeline(name: str, artifact: Input[Artifact]) -> None:
+    def test_pipeline(name: str, artifact: Input[Artifact]) -> None:
         dummy_component(name=name, artifact=artifact)
 
-    return dummy_pipeline
+    return test_pipeline
+
+
+@pytest.fixture
+def registry_client_response():
+    with open("tests/samples/registry_client_response.pkl", "rb") as f:
+        return pickle.load(f)  # noqa: S301
