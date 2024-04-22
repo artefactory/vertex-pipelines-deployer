8ae17d6d1539960baa4dbf21d70540bba35ca16c
Elie Trigano <elie.trigano@artefact.com>
2024-04-22 17:57:02 +0200

    check no None in pyproject

--- tests/integration_tests/test_init.py
+++ tests/integration_tests/test_init.py
@@ -76,13 +76,16 @@ def test_init_command_with_user_input(tmp_path):
         # Pay attention to the assertions if the user inputs are changed
         assert Path("pyproject.toml").is_file()
         parsed_toml = toml.loads(Path("pyproject.toml").read_text())
+        for section in parsed_toml.values():
+            for key, value in section.items():
+                assert value not in [None, "None"], f"Found None for key: {key}"
+        assert "env_file" not in parsed_toml["tool"]["vertex_deployer"]["deploy"]
+        assert "cron" not in parsed_toml["tool"]["vertex_deployer"]["deploy"]
+        assert "check" not in parsed_toml["tool"]["vertex_deployer"]
         assert parsed_toml["tool"]["vertex_deployer"]["vertex_folder_path"] == "custom_value"
         assert parsed_toml["tool"]["vertex_deployer"]["deploy"]["upload"] is True
         assert parsed_toml["tool"]["vertex_deployer"]["deploy"]["compile"] is False
         assert parsed_toml["tool"]["vertex_deployer"]["create"]["config_type"] == "json"
-        assert "env_file" not in parsed_toml["tool"]["vertex_deployer"]["deploy"]
-        assert "cron" not in parsed_toml["tool"]["vertex_deployer"]["deploy"]
-        assert "check" not in parsed_toml["tool"]["vertex_deployer"]
         assert Path("custom_value").is_dir()
         assert (Path("custom_value") / "pipelines" / "pipe.py").is_file()
         assert (Path("custom_value") / "configs" / "pipe" / "test.json").is_file()
